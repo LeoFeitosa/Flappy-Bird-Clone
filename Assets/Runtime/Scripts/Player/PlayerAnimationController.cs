d2dd8e257320bfd753e791510c5de88834092290
unknown <ffeitosa.leonardo@gmail.com>
2021-10-19 19:17:21 -0300

    config player

---
+++ Assets/Runtime/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private PlayerController playerController;
+    private static readonly int velocityYId = Animator.StringToHash("SpeedY");
+
+    void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    void LateUpdate()
+    {
+        animator.SetFloat(velocityYId, playerController.Velocity.y);
+    }
+}
---
+++ Assets/Runtime/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public bool Tap()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            return true;
+        }
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
